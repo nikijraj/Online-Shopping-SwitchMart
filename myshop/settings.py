c2dac25d99f25979fbef2f9e1192ddd2d5b81b62
nikijraj <nikijraj@gmail.com>
2021-04-12 06:31:10 +0530

    Multiselect benched (temporary)

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'switchmart.apps.SwitchmartConfig',
-	'multiselectfield',
+#	'multiselectfield',
 ]
 
 MIDDLEWARE = [
